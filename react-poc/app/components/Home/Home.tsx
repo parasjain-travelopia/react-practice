4f3685b89ed9d6074d8c64aa5773462256008bb5
parasjain-travelopia <147372590+parasjain-travelopia@users.noreply.github.com>
2025-02-16 12:11:33 +0530

    add about section

--- react-poc/app/components/Home/Home.tsx
+++ react-poc/app/components/Home/Home.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import Hero from "./Hero/Hero";
+import About from "./About/About";
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero />
+      <About />
     </div>
   );
 };
